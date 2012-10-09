1e60ca718eb01c39d804cd30e68d7b47dfeeb6cd
sokutou-metsu <sokutou.metsu@gmail.com>
2012-10-10 00:16:48 +0900

    演習問題2-2

--- python/thinkstats/first.py
+++ python/thinkstats/first.py
@@ -6,8 +6,10 @@ u"""演習問題 1-3 "Think Stats"
  * survey.py
 """
 
+import math
 import survey
 import sys
+from thinkstats import Mean, Var
 
 
 def is_outcome(record):
@@ -25,17 +27,12 @@ def is_not_first_birth(record):
     return is_outcome(record) and record.birthord != 1
 
 
-def mean(data):
-    u"""算術平均を求める"""
-    return float(sum(data)) / len(data)
-
-
 def fetch_avg_prglength(table, cond=is_outcome):
     u"""条件に合うレコードの数と妊娠期間を求める"""
     match = survey.Pregnancies()
     match.ExtendRecords(filter(cond, table.records))
-    avg_prglength = mean(map(lambda r: r.prglength, match.records))
-    return (len(match), avg_prglength)
+    prglength = map(lambda r: r.prglength, match.records)
+    return (len(match), Mean(prglength), math.sqrt(Var(prglength)))
 
 
 def main(name, data_dir='.'):
@@ -47,12 +44,14 @@ def main(name, data_dir='.'):
     print u'生児出生数： ', outcome[0]
 
     first_birth = fetch_avg_prglength(table, is_first_birth)
-    print u'第一子： ', first_birth[1], first_birth[0]
+    print u'第一子： ', first_birth[1], first_birth[0], first_birth[2]
 
     the_other = fetch_avg_prglength(table, is_not_first_birth)
-    print u'第二子以降： ', the_other[1], the_other[0]
+    print u'第二子以降： ', the_other[1], the_other[0], the_other[2]
 
-    print u'妊娠日数の差： ', (first_birth[1] - the_other[1]) * 7
+    diff_mean = (first_birth[1] - the_other[1]) * 7
+    diff_std_dev = first_birth[2] - the_other[2]
+    print u'妊娠日数の差： ', diff_mean, diff_std_dev
 
     return 0
 
