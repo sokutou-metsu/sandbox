47e1d04c67c31476f141f3fa6adce46bfd4d7ebd
sokutou-metsu <sokutou.metsu@gmail.com>
2012-10-21 00:22:41 +0900

    演習問題3-4

--- python/thinkstats/exercise-3-3.py
+++ python/thinkstats/exercise-3-3.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8-unix -*-
-u"""演習問題 3-3 "Think Stats"
+u"""演習問題 3-3、3-4 "Think Stats"
 """
 
 import sys
@@ -22,15 +22,83 @@ def Percentile(scores, percentile_rank):
     u"""任意のパーセンタイル順位に対応する点数を返す
     """
     scores.sort()
-    index = percentile_rank * (len(scores) - 1) / 100
+    index = int(percentile_rank * (len(scores) - 1) / 100)
     return scores[index]
+
+
+def Partition(l, left, right, pivot_index):
+    u"""リストをある値より小さい数のリストと
+    それより大きい数のリストに分割する
+
+    Wikipedia 『選択アルゴリズム』 より
+    """
+    def swap(i, j):
+        tmp = l[i]
+        l[i] = l[j]
+        l[j] = tmp
+
+    pivot_value = l[pivot_index]
+    swap(pivot_index, right)
+    store_index = left
+    for i in range(left, right):
+        if l[i] <= pivot_value:
+            swap(store_index, i)
+            store_index = store_index + 1
+    swap(right, store_index)
+    return store_index
+
+
+def Select(target_list, k):
+    u"""リストの要素の中でk番目に小さい値を求める
+
+    Wikipedia 『選択アルゴリズム』 より
+    """
+    def Median(l):
+        m = l[:]
+        m.sort()
+        return l.index(m[len(l) / 2])
+
+    def SelecpPivot(l, left, right):
+        size = len(l) / 5
+        return Median([Median(l[i:i+size]) for i in range(0, len(l), size)])
+
+    l = target_list[:]
+    left = 0
+    right = len(l) - 1
+
+    while True:
+        pivot_index = SelecpPivot(l, left, right)
+        pivot_new_index = Partition(l, left, right, pivot_index)
+
+        if k == pivot_new_index:
+            return l[k]
+        elif k < pivot_new_index:
+            right = pivot_new_index - 1
+        else:
+            left = pivot_new_index + 1
+
+
+def PercentileWithSelectAlgorithm(scores, percentile_rank):
+    u"""選択アルゴリズムを使って
+    任意のパーセンタイル順位に対応する点数を返す
+    """
+    index = 80 * (len(scores) - 1) / 100
+    return Select(scores, index)
 
 
 class TestSample(unittest.TestCase):
 
-    def test_case(self):
+    def test_case1(self):
         scores = [55, 56, 77, 88, 99]
-        self.assertEquals(Percentile(scores, 80), 88)
+        percentile_rank = PercentileRank(scores, 88)
+        self.assertEquals(Percentile(scores, percentile_rank), 88)
+        return
+
+    def test_case2(self):
+        scores = [55, 56, 77, 88, 99]
+        percentile_rank = PercentileRank(scores, 88)
+        self.assertEquals(
+            PercentileWithSelectAlgorithm(scores, percentile_rank), 88)
         return
 
 
