99546ad6bfda20a2581418360830ff5eed140ad7
sokutou-metsu <sokutou.metsu@gmail.com>
2013-01-31 01:22:07 +0900

    テストケース追加

--- ocaml/sample/test/utility/testFold.ml
+++ ocaml/sample/test/utility/testFold.ml
@@ -3,17 +3,41 @@
 open Utility.List
 open OUnit
 
+let string_of_int_list int_list =
+  "[" ^ (String.concat "; " (List.map string_of_int int_list)) ^ "]"
+
 let test_fold () =
-  assert_equal [3; 2; 1] (fold (fun x y -> x :: y) [] [1; 2; 3]);
-  assert_equal 6 (fold (fun x y -> x * y) 1 [1; 2; 3])
+  assert_equal
+    ~printer:string_of_int_list
+    [3; 2; 1] (fold (fun x y -> x :: y) [] [1; 2; 3]);
+  assert_equal
+    ~printer:string_of_int
+    6 (fold (fun x y -> x * y) 1 [1; 2; 3]);
+  assert_equal
+    ~printer:string_of_int
+    (-98) (fold (fun x y -> x - y) 100 [1; 2; 3])
 
 let test_fold_left () =
-  assert_equal [3; 2; 1] (fold_left (fun x y -> y :: x) [] [1; 2; 3]);
-  assert_equal 6 (fold_left (fun x y -> x * y) 1 [1; 2; 3])
+  assert_equal
+    ~printer:string_of_int_list
+    [3; 2; 1] (fold_left (fun x y -> y :: x) [] [1; 2; 3]);
+  assert_equal
+    ~printer:string_of_int
+    6 (fold_left (fun x y -> x * y) 1 [1; 2; 3]);
+  assert_equal
+    ~printer:string_of_int
+    94 (fold_left (fun x y -> x - y) 100 [1; 2; 3])
 
 let test_fold_right () =
-  assert_equal [1; 2; 3] (fold_right (fun x y -> x :: y) [] [1; 2; 3]);
-  assert_equal 6 (fold_right (fun x y -> x * y) 1 [1; 2; 3])
+  assert_equal
+    ~printer:string_of_int_list
+    [1; 2; 3] (fold_right (fun x y -> x :: y) [] [1; 2; 3]);
+  assert_equal
+    ~printer:string_of_int
+    6 (fold_right (fun x y -> x * y) 1 [1; 2; 3]);
+  assert_equal
+    ~printer:string_of_int
+    (-98) (fold_right (fun x y -> x - y) 100 [1; 2; 3])
 
 let suite =
   "Test Fold" >::: [
